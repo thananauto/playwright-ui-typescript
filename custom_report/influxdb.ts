27b91928fc1c474b11079f525a81d758da4a698f
thananJayan <thananjayan1988@gmail.com>
2024-06-11 23:47:33 +0000

    Updated with Influx DB details

---
+++ custom_report/influxdb.ts
@@ -0,0 +1,28 @@
+import {InfluxDB, Point} from '@influxdata/influxdb-client'
+
+  class InfluxDb{
+   
+    readonly writeApi 
+  
+    constructor(){
+        this.writeApi = new InfluxDB( { url : process.env.INFLUX_URL as string,  token :  process.env.INFLUX_TOKEN as string})
+                        .getWriteApi(process.env.INFLUX_ORG as string,   process.env.INFLUX_BUCKET as string)
+     
+    }
+
+    async writeMeasurement(point: Point){
+         await this. writeApi.writePoint(point)
+
+    }
+
+
+    async closeApi(){
+        await this.writeApi.close().then(() => {
+            console.log('WRITE FINISHED')
+          })
+    }
+
+}
+
+const influx = new InfluxDb()
+export { influx, Point}
