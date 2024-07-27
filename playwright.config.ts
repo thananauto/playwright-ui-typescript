37a1caf67d22555eb2786ef8613a3f96193d9ef1
thananJayan <thananjayan1988@gmail.com>
2024-07-27 13:09:14 +0000

    Create test report with different timestamp

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 // require('dotenv').config();
 
 dotenv.config();
-
+const timestamp = (new Date(Date.now()).toUTCString()).replace(/ /gi,'_').replace(/:/gi,'').replace(/,/gi,'')
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */ 
-  reporter: [["html"], ['list'], ["./custom_report/customreporter.ts"]],
+  reporter: [["html",{outputFolder: `./playwright-report/${timestamp}`], ['list'], ["./custom_report/customreporter.ts"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
